71322aceb238d755e33e1606265998847ff04c0e
Oleksii Khodus <exalchel@gmail.com>
2026-05-23 01:16:44 +0300

    fix: check format

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -127,7 +127,9 @@ export default function ProfileScreen({ navigation }: ProfileScreenProps) {
                 ]}
               >
                 <MaterialCommunityIcons
-                  name={attr.icon as keyof typeof MaterialCommunityIcons.glyphMap}
+                  name={
+                    attr.icon as keyof typeof MaterialCommunityIcons.glyphMap
+                  }
                   size={24}
                   color={attr.color}
                 />
